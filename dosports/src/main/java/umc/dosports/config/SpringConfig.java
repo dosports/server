99b1dc863b68155d0fa042d2866c00fc7667766c
jeayoungKim529 <jeay529@gmail.com>
2022-08-23 21:26:31 +0900

    [FIX] SpringConfig @Bean like

--- dosports/src/main/java/umc/dosports/config/SpringConfig.java
+++ dosports/src/main/java/umc/dosports/config/SpringConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import umc.dosports.Comment.CommentRepository;
+import umc.dosports.Comment.CommentService;
+import umc.dosports.Comment.JdbcTemplateCommentRepository;
+import umc.dosports.Like.JdbcTemplateLikeRepository;
+import umc.dosports.Like.LikeRepository;
+import umc.dosports.Like.LikeService;
+import umc.dosports.Notification.JdbcTemplateNotiRepository;
+import umc.dosports.Notification.NotiRepository;
+import umc.dosports.Notification.NotiService;
 import umc.dosports.Review.JdbcTemplateReviewRepository;
 import umc.dosports.Review.ReviewRepository;
 import umc.dosports.Review.ReviewService;
@@ -34,6 +43,10 @@ public class SpringConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+    @Bean
+    public LikeService LikeService() {
+        return new LikeService(likeRepository());
+    }
 
     @Bean
     public UserRepository userRepository() {
@@ -44,7 +57,7 @@ public class SpringConfig {
         return new JdbcTemplateReviewRepository(dataSource);
     }
     @Bean
-    public TokenProvider tokenProvider() {
-        return new TokenProvider();
-    }
+    public TokenProvider tokenProvider() { return new TokenProvider(); }
+    @Bean
+    public LikeRepository likeRepository() { return new JdbcTemplateLikeRepository(dataSource); }
 }
